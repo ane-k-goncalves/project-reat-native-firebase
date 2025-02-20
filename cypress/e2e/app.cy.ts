fad661d4dd9f95da29884b6cfdf7ab6ac07c4775
ane-k-goncalves <goncalvesanne961@gmail.com>
2025-02-20 19:46:16 -0300

    att

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -14,18 +14,17 @@ describe('template spec', () => {
   it('Deve impedir login se o email ou senha estiverem vazios', () => {
     cy.visit("http://localhost:8081");
     
-    // Deixa os campos de email e senha vazios
+    
     cy.get('input[placeholder="email"]').clear();
     cy.get('input[placeholder="password"]').clear();
     
     cy.contains('Login').click();
     
-    // Verifica se o alerta de erro é exibido
+   
     cy.on('window:alert', (str) => {
       expect(str).to.include('Por favor, preencha ambos os campos.');
     });
     
-    // Verifica se a aplicação permanece na tela de login
     cy.url().should('include', 'http://localhost:8081');
   });
 })
--- src/__tests__/UseAuth.test.tsx
+++ src/__tests__/UseAuth.test.tsx
@@ -29,29 +29,29 @@ describe("useAuth Hook", () => {
   it('enviar e-mail e senha ao pressionar o botão de login', async () => {
     const loginMock = jest.fn().mockResolvedValue(undefined);
 
-    // Mock do hook useAuth para simular estado de carregamento e a função login
+   
     (useAuth as jest.Mock).mockReturnValue({
       login: loginMock,
       user: null,
-      loading: false,  // Simula que o loading acabou
+      loading: false, 
     })
 
     const { getByPlaceholderText, getByText } = render(<Index />);
 
-    // Aguarda até que os campos de entrada estejam disponíveis
+   
     await waitFor(() => {
       expect(getByPlaceholderText('email')).toBeTruthy();
       expect(getByPlaceholderText('password')).toBeTruthy();
     });
 
-    // Preenche os campos de email e senha
+    
     fireEvent.changeText(getByPlaceholderText('email'), 'test@example.com');
     fireEvent.changeText(getByPlaceholderText('password'), 'password123');
 
-    // Simula o clique no botão de login
+    
     fireEvent.press(getByText('Login'));
 
-    // Verifica se a função login foi chamada com os valores corretos
+    
     expect(loginMock).toHaveBeenCalledWith('test@example.com', 'password123');
   });
   
